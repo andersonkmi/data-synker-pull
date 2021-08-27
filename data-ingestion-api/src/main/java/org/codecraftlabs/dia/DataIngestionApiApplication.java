1eae2debba8d22f3f6d31e5837b2fd01f4f145ad
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-08-27 20:58:47 +0200

    Initial commit of the first application

---
+++ data-ingestion-api/src/main/java/org/codecraftlabs/dia/DataIngestionApiApplication.java
@@ -0,0 +1,13 @@
+package org.codecraftlabs.dia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIngestionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIngestionApiApplication.class, args);
+	}
+
+}
---
+++ data-ingestion-api/src/test/java/org/codecraftlabs/dia/DataIngestionApiApplicationTests.java
@@ -0,0 +1,15 @@
+package org.codecraftlabs.dia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DataIngestionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
