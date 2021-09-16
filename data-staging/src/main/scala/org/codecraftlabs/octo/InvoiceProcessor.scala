39fb0a916c67b760e8afdf82a75e4520bade908c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-09-16 22:04:16 +0200

    more code

--- data-staging/src/main/scala/org/codecraftlabs/octo/InvoiceProcessor.scala
+++ data-staging/src/main/scala/org/codecraftlabs/octo/InvoiceProcessor.scala
@@ -1,13 +1,24 @@
 package org.codecraftlabs.octo
 
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.amazonaws.services.lambda.runtime.events.S3Event
-import org.apache.logging.log4j.{LogManager, Logger}
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import org.apache.log4j.{LogManager, Logger}
+
+import scala.jdk.CollectionConverters._
 
 class InvoiceProcessor extends RequestHandler [S3Event, String] {
   private val logger: Logger = LogManager.getLogger(getClass)
 
   override def handleRequest(input: S3Event, context: Context): String = {
+    logger.info("Processing json file")
+    input.getRecords.asScala.foreach(item => readObject(item))
     "Done"
+  }
+
+  private def readObject(event: S3EventNotification.S3EventNotificationRecord): Unit = {
+    val bucket = event.getS3.getBucket.getName
+    val key = event.getS3.getObject.getKey;
+    logger.info(s"Processing object $key from bucket $bucket");
   }
 }
