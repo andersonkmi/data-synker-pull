6453308d8ee8c689da877f26d482fe09e8697734
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-09-28 22:48:13 +0200

    Minor refactoring

--- data-staging/src/main/scala/org/codecraftlabs/octo/model/InvoiceTracking.scala
+++ data-staging/src/main/scala/org/codecraftlabs/octo/model/InvoiceTracking.scala
@@ -1,13 +1,14 @@
 package org.codecraftlabs.octo.model
 
-case class InvoiceTracking(invoicePartition: String,
+case class InvoiceTracking(
+                            invoicePartition: String,
                             invoiceTrackingNumber: Long,
-                           requestType: String,
-                           timestamp: Long,
-                           invoiceId: String,
-                           name: Option[String],
-                           amount: Option[Double],
-                           companyName: Option[String],
-                           billToName: Option[String],
-                           invoiceStatus: Option[String],
-                           trackingStatus: String)
+                            requestType: String,
+                            timestamp: Long,
+                            invoiceId: String,
+                            name: Option[String],
+                            amount: Option[Double],
+                            companyName: Option[String],
+                            billToName: Option[String],
+                            invoiceStatus: Option[String],
+                            trackingStatus: String)
