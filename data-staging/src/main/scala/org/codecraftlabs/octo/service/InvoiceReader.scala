a3d0cb35864a5c71a9c5e226c8dc3db478397faf
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-09-28 21:50:14 +0200

    More code

--- data-staging/src/main/scala/org/codecraftlabs/octo/service/InvoiceReader.scala
+++ data-staging/src/main/scala/org/codecraftlabs/octo/service/InvoiceReader.scala
@@ -41,7 +41,7 @@ object InvoiceReader {
     request match {
       case CREATE => Some(extractInvoiceCreateOrUpdateJsonFields(json, CREATE))
       case UPDATE => Some(extractInvoiceCreateOrUpdateJsonFields(json, UPDATE))
-      case PATCH => Some(extractInvoicePatchJsonFields(json))
+      case PATCH =>  Some(extractInvoicePatchJsonFields(json))
       case DELETE => Some(extractInvoiceDeleteJsonFields(json))
       case _ => None
     }
